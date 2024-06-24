18fbdf877432d57b44015049667a3c19aa7d4710
ЦИТ РТ-Тимерзянов Динар Рустемович <Dinar.Timerzyanov@tatar.ru>
2024-06-24 13:34:57 +0300

    first commit

---
+++ src/main/java/ru/sysout/websocket/WebsocketApplication.java
@@ -0,0 +1,13 @@
+package ru.sysout.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketApplication.class, args);
+	}
+
+}
